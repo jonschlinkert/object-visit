a62f8d8ec3053c3ada4feb4bb1b49790c803bfd6
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-06 09:49:20 -0400

    better type checking

--- index.js
+++ index.js
@@ -8,19 +8,28 @@
 'use strict';
 
 var isObject = require('isobject');
+var expected - require('expected');
 
 module.exports = function visit(thisArg, method, target) {
   if (!isObject(thisArg) && typeof thisArg !== 'function') {
-    throw new Error('object-visit expects `thisArg` to be an object.');
+    expected(thisArg).to.be.an('object');
+  }
+
+  if (!isObject(target) && typeof target !== 'function') {
+    expected(target).to.be.an('object');
   }
 
   if (typeof method !== 'string') {
-    throw new Error('object-visit expects `method` name to be a string');
+    expected(method).to.be.a('string');
   }
 
   target = target || {};
   for (var key in target) {
-    thisArg[method](key, target[key]);
+    var fn = thisArg[method];
+    if (typeof fn !== 'function') {
+      expected(fn).to.be.a('function');
+    }
+    fn(key, target[key]);
   }
   return thisArg;
 };
